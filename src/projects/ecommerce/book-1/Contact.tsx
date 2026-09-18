d751cc6534d9dd0c6f99769b6ccdba773e4cfa50
Peter <easylivetechsolution@gmail.com>
2026-09-18 03:33:42 +0100

    update

--- src/projects/ecommerce/book-1/Contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ProjectTemplate from '../../../components/ProjectTemplate';
-
-export const Home = () => <ProjectTemplate title="ECOMMERCE - BOOK 1 Home" />;
-export const About = () => <ProjectTemplate title="ECOMMERCE - BOOK 1 About" />;
-export const Services = () => <ProjectTemplate title="ECOMMERCE - BOOK 1 Services" />;
-export const Contact = () => <ProjectTemplate title="ECOMMERCE - BOOK 1 Contact" />;
